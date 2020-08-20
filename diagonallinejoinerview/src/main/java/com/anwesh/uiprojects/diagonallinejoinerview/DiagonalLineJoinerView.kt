29186061c71cea70049595e614627ac2765f25b3
Anwesh43 <anweshthecool0@gmail.com>
2020-08-21 04:37:34 +0530

    adding DLJNode class

--- diagonallinejoinerview/src/main/java/com/anwesh/uiprojects/diagonallinejoinerview/DiagonalLineJoinerView.kt
+++ diagonallinejoinerview/src/main/java/com/anwesh/uiprojects/diagonallinejoinerview/DiagonalLineJoinerView.kt
@@ -117,4 +117,45 @@ class DiagonalLineJoinerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DLJNode(var i : Int, val state : State = State()) {
+
+        private var next : DLJNode? = null
+        private var prev : DLJNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = DLJNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DLJNode {
+            var curr : DLJNode? = next
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
